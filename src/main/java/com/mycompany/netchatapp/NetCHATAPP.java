4f7b9b97b1faab467c57fa44e23cac1e7f1ae0d4
msi-code <manxiwamsimelelo@gmail.com>
2026-05-25 17:49:50 +0200

    Initial commit

---
+++ src/main/java/com/mycompany/netchatapp/NetCHATAPP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.netchatapp;
+
+/**
+ *
+ * @author MANXIWA MSIMELELO
+ */
+public class NetCHATAPP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
